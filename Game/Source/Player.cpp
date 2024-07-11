9b0bf4f4a3d6b8111246b0385b65f54f4bc2776c
AlCh440 <73243813+AlCh440@users.noreply.github.com>
2024-07-11 20:36:53 +0200

    Trying to fix player

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -102,7 +102,7 @@ bool Player::Start()
 	hit_player = app->coll->AddCollider(player, Collider::Type::PLAYER, 0, app->player);
 	near_right = app->coll->AddCollider({ player.x + player.w, player.y, 1, player.h - 1 }, Collider::Type::NEAR, 0, app->player);
 	near_left = app->coll->AddCollider({ player.x - 1, player.y, 1, player.h - 1 }, Collider::Type::NEAR, 0, app->player);
-	near_down = app->coll->AddCollider({ player.x, player.y + player.w + 3, player.w, 4 }, Collider::Type::NEAR, 0, app->player);
+	near_down = app->coll->AddCollider({ player.x, player.y + player.w + 7, player.w, 4 }, Collider::Type::NEAR, 0, app->player);
 
 	current_animation = &right_running;
 
@@ -201,8 +201,11 @@ bool Player::Update(float dt)
 	}
 	else
 	{
-		direction = 0;
-		state = IDLE;
+		if (momentum.y > 0)
+		{
+			direction = 0;
+			state = IDLE;
+		}
 	}
 	
 	
